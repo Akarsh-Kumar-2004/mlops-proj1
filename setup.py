f9c4e2087208d68f4cb72bf349fa92ed2bfecd7b
Akarsh <akarshkumar2004@gmail.com>
2025-12-24 21:51:17 +0530

    mongodb setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akarsh Kumar",
+    author_email="akarshkumar2004@gmail.com",
+    packages=find_packages()
+)
